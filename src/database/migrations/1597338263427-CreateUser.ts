0c44e59d21adaa1cdc566c3d04f5d79325369b82
kleyson castro <kleysoncastro99@gmail.com>
2020-08-13 13:40:18 -0400

    Feat: create migration for users 	was create model/migration for user 	add function for creted_at and updated_at model Appointiment

--- src/database/migrations/1597338263427-CreateUser.ts
+++ src/database/migrations/1597338263427-CreateUser.ts
@@ -1,12 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
-import { tr } from 'date-fns/locale';
 
-export default class CreateAppointments1597285304562
-  implements MigrationInterface {
+export default class CreateUser1597338263427 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -16,14 +14,17 @@ export default class CreateAppointments1597285304562
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
-            isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
-            isNullable: false,
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -41,6 +42,6 @@ export default class CreateAppointments1597285304562
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
