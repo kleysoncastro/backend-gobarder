47b591d62e43e7ca94b5552d95ebe7a32d12b0a3
kleyson castro <kleysoncastro99@gmail.com>
2020-08-12 22:36:53 -0400

    Config: config typesorm completed

---
+++ src/database/migrations/1597285304562-CreateAppointments.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateAppointments1597285304562 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
