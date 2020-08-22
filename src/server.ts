b23cb7ae2f3c09ff9a63481c2c0b2f3b56a0d7fd
kleyson castro <kleysoncastro99@gmail.com>
2020-08-22 00:27:48 -0400

    chore: route for see file 	route /file for show avatar

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes/index';
-
+import uploadConfig from './config/uploads';
 import './database';
 
 const app = express();
 app.use(express.json());
+app.use('/file', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
